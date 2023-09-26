95cc51f0c6f49437dbf7ba4163f8f1983c846e26
Ali12hamdan <Ali12hamdan@gmail.com>
2023-09-26 18:59:56 +0200

    add meta

--- app.vue
+++ app.vue
@@ -5,3 +5,36 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script lang="ts" setup>
+useHead({
+  title: "TechBlog",
+  meta: [
+    { hid: "og:title", property: "og:title", content: "TechBlog" },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: "https://tech-blog-kappa-nine.vercel.app/images/favicon.png",
+    },
+
+    // telegram card
+    {
+      hid: "telegram:title",
+      name: "telegram:title",
+      content: "TechBlog",
+    },
+
+    {
+      hid: "telegram:description",
+      name: "telegram:description",
+      content:
+        "The website is a blog that focuses on various technical subjects. Itprovides informative and insightful articles covering a wide range of topics related to technology, programming, and engineering. Readers can expect to find detailed tutorials, tips, and tricks, as well as in-depth analysis of emerging trends and innovations in the field",
+    },
+    {
+      hid: "telegram:image",
+      name: "telegram:image",
+      content: "https://tech-blog-kappa-nine.vercel.app/images/favicon.png",
+    },
+  ],
+});
+</script>
